14854c401003d45699b58cc03ae98bc57a270746
caupolicanre <caupolicanre@gmail.com>
2025-11-29 10:37:56 -0300

    chore: configure settings vars

---
+++ settings.js
@@ -0,0 +1,6 @@
+functionGlobalContext: {
+    API_BASE_URL: "https://api.jolpi.ca/ergast/f1",
+    CURRENT_SEASON: "2024",
+    REQUEST_LIMIT: "30",
+    CACHE_DURATION: 300000  // 5 minutos en milisegundos
+}
